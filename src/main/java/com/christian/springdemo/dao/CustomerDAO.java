b8b0b0859eed2ae2f3ab1e531a20889581536c71
Christian <christian@sanglech.com>
2021-04-24 01:23:50 +0900

    Initial Commit of CRM-REST-Project

---
+++ src/main/java/com/christian/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package com.christian.springdemo.dao;
+
+import java.util.List;
+
+import com.christian.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
